1987205c349be47979d70362f5f317c9bf94064c
이용재 <dydwo0740@iyongjaeui-MacBookPro.local>
2023-07-04 21:04:32 +0900

    mission4 test start

--- src/main/java/softeer2nd/chess/Board.java
+++ src/main/java/softeer2nd/chess/Board.java
@@ -6,6 +6,8 @@ import softeer2nd.utils.StringUtils;
 
 import java.util.*;
 
+import static softeer2nd.utils.StringUtils.*;
+
 /**
  * 초기에는 char 이차원 배열을 통한 구현
  * 이를 대체하기 위해서, ArrayList 를 생각했지만 추후에 기물의 이동 수정에서 문제가 발생할 것 같다.
@@ -16,9 +18,6 @@ public class Board {
     private List<Piece> store = new ArrayList<>();
 
     private Map<Location, Piece> state = new HashMap<>();
-
-
-
 
 
     public List<Piece> getStore() {
@@ -34,43 +33,58 @@ public class Board {
     }
 
     public String getWhitePawnsResult() {
-        StringBuilder sb = new StringBuilder();
-
-        return sb.toString();
+        return findByPawnColor(Piece.createWhitePawn());
     }
 
     public String getBlackPawnsResult() {
-        StringBuilder sb = new StringBuilder();
-
-        return sb.toString();
+        return findByPawnColor(Piece.createBlackPawn());
     }
 
-    public void initialize(){
-        for(int i=0;i<8;i++){
-            for(int j=0;j<8;j++){
-                if(i == 1){
+    public void initialize() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (i == 1) {
                     state.put(new Location(i, j), Piece.createBlackPawn());
-                }else if(i == 6){
+                } else if (i == 6) {
                     state.put(new Location(i, j), Piece.createWhitePawn());
-                }else {
+                } else if (i == 0) {
+                    state.put(new Location(i, 0), Piece.createBlackRook());
+                    state.put(new Location(i, 7), Piece.createBlackRook());
+                    state.put(new Location(i, 1), Piece.createBlackKnight());
+                    state.put(new Location(i, 6), Piece.createBlackKnight());
+                    state.put(new Location(i, 2), Piece.createBlackBishop());
+                    state.put(new Location(i, 5), Piece.createBlackBishop());
+                    state.put(new Location(i, 3), Piece.createBlackQueen());
+                    state.put(new Location(i, 4), Piece.createBlackKing());
+                } else if (i == 7) {
+                    state.put(new Location(i, 0), Piece.createWhiteRook());
+                    state.put(new Location(i, 7), Piece.createWhiteRook());
+                    state.put(new Location(i, 1), Piece.createWhiteKnight());
+                    state.put(new Location(i, 6), Piece.createWhiteKnight());
+                    state.put(new Location(i, 2), Piece.createWhiteBishop());
+                    state.put(new Location(i, 5), Piece.createWhiteBishop());
+                    state.put(new Location(i, 3), Piece.createWhiteQueen());
+                    state.put(new Location(i, 4), Piece.createWhiteKing());
+                }
+                else {
                     state.put(new Location(i, j), Piece.createEmptyPawn());
                 }
             }
         }
-        
+
 
     }
 
-    public void print(){
+    public void print() {
         StringBuilder sb = new StringBuilder();
         initialize();
 
-        for(int i=0;i<8;i++){
+        for (int i = 0; i < 8; i++) {
             String str = "";
-            for(int j=0;j<8;j++){
+            for (int j = 0; j < 8; j++) {
                 str += String.valueOf(state.get(new Location(i, j)));
             }
-            sb.append(StringUtils.appendNewLine(str));
+            sb.append(appendNewLine(str));
         }
 
 
@@ -78,27 +92,46 @@ public class Board {
 
     }
 
-    public String findByPawnColor(final Piece findPiece){
+    public String findByPawnColor(final Piece findPiece) {
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<8;i++){
-            for(int j=0;j<8;j++){
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
                 Piece piece = state.get(new Location(i, j));
-                if(piece.getColor().equals(findPiece.getColor()) && piece.getRepresentation() == findPiece.getRepresentation()){
+                if (piece.getColor().equals(findPiece.getColor()) && piece.getRepresentation() == findPiece.getRepresentation()) {
                     sb.append(piece.getRepresentation());
                 }
             }
         }
 
-        return StringUtils.appendNewLine(sb.toString());
+        return appendNewLine(sb.toString());
     }
 
     public int pieceCount() {
         int count = 0;
-        for(int i=0;i<8;i++){
-            for(int j=0;j<8;j++){
-                if(state.get(new Location(i, j)).get)
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (!state.get(new Location(i, j)).getName().equals(EMPTY)) {
+                    count++;
+                }
             }
         }
+
+        return count;
+
+    }
+
+    public String showBoard() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 8; i++) {
+            String str = "";
+            for (int j = 0; j < 8; j++) {
+                str += String.valueOf(state.get(new Location(i, j)).getRepresentation());
+            }
+            sb.append(appendNewLine(str));
+        }
+
+        return sb.toString();
 
     }
 }
--- src/main/java/softeer2nd/chess/pieces/Piece.java
+++ src/main/java/softeer2nd/chess/pieces/Piece.java
@@ -43,50 +43,50 @@ public class Piece {
     public static Piece createWhitePawn(){
         //굳이 매개변수로 색깔을 넘겨줄 필요가 없다.
         // whitePawn이라고 명시되어있기 때문입니다.
-        Piece piece = new Piece(WHITE, WHITE_PAWN);
+        Piece piece = new Piece(WHITE,PAWN_NAME, WHITE_PAWN);
         return piece;
     }
 
     public static Piece createBlackPawn(){
-        Piece piece = new Piece(BLACK, BLACK_PAWN);
+        Piece piece = new Piece(BLACK, PAWN_NAME, BLACK_PAWN);
         return piece;
     }
 
     public static Piece createEmptyPawn(){
-        Piece piece = new Piece(EMPTY, EMPTY_REPRESENTATION);
+        Piece piece = new Piece(EMPTY, EMPTY, EMPTY_REPRESENTATION);
         return piece;
     }
 
-    public static Piece createdWhiteKnight(){
+    public static Piece createWhiteKnight(){
         return new Piece(WHITE, KNIGHT_NAME, WHITE_KNIGHT);
     }
 
-    public static Piece createdBlackKnight(){
+    public static Piece createBlackKnight(){
         return new Piece(BLACK, KNIGHT_NAME, BLACK_KNIGHT);
     }
 
-    public static Piece createdWhiteRook(){
+    public static Piece createWhiteRook(){
         return new Piece(WHITE, ROOK_NAME, WHITE_ROOK);
     }
-    public static Piece createdBlackRook(){
+    public static Piece createBlackRook(){
         return new Piece(BLACK, ROOK_NAME, BLACK_ROOK);
     }
-    public static Piece createdWhiteBishop(){
+    public static Piece createWhiteBishop(){
         return new Piece(WHITE, BISHOP_NAME, WHITE_BISHOP);
     }
-    public static Piece createdBlackBishop(){
+    public static Piece createBlackBishop(){
         return new Piece(BLACK, BISHOP_NAME, BLACK_BISHOP);
     }
-    public static Piece createdWhiteQueen(){
+    public static Piece createWhiteQueen(){
         return new Piece(WHITE, QUEEN_NAME, WHITE_QUEEN);
     }
-    public static Piece createdBlackQueen(){
+    public static Piece createBlackQueen(){
         return new Piece(BLACK, QUEEN_NAME, BLACK_QUEEN);
     }
-    public static Piece createdWhiteKing(){
+    public static Piece createWhiteKing(){
         return new Piece(WHITE, KING_NAME, WHITE_KING);
     }
-    public static Piece createdBlackKing(){
+    public static Piece createBlackKing(){
         return new Piece(BLACK, KING_NAME, BLACK_KING);
     }
 }
