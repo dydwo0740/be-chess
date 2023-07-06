c5debcd64350e06fc4cfb344061e37e217cf8e7e
이용재 <dydwo0740@iyongjaeui-MacBookPro.local>
2023-07-06 15:38:54 +0900

    create Pawn class

--- src/main/java/softeer2nd/chess/pieces/PieceFactory.java
+++ src/main/java/softeer2nd/chess/pieces/PieceFactory.java
@@ -1,4 +1,26 @@
 package softeer2nd.chess.pieces;
 
+import static softeer2nd.chess.pieces.Chess.*;
+import static softeer2nd.chess.pieces.Chess.Color.*;
+
 public class PieceFactory {
+    public static Chess createChess(Color color, Type type){
+        if(color.equals(WHITE)){
+
+        }else if(color.equals(BLACK)){
+
+        }else{
+
+        }
+    }
+
+    public static Chess createWhite(Type type) {
+        switch (type) {
+            case PAWN:
+                return
+        }
+    }
+    public static Chess createBlack(Type type) {
+
+    }
 }
