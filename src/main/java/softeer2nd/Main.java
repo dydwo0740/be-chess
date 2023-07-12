455c45798a7c62f7c2002a37edda695571177652
dydwo0740 <dydwo0740@naver.com>
2023-07-12 11:00:33 +0900

    seperate class ChessStarter

--- src/main/java/softeer2nd/Main.java
+++ src/main/java/softeer2nd/Main.java
@@ -2,6 +2,7 @@ package softeer2nd;
 
 import softeer2nd.chess.Board;
 import softeer2nd.chess.exception.NotUserTurn;
+import softeer2nd.chess.game.ChessStarter;
 import softeer2nd.chess.game.GameChess;
 import softeer2nd.chess.view.GameView;
 
@@ -10,37 +11,7 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        Board board = new Board();
-        GameChess gameChess = new GameChess(board);
-        GameView gameView = new GameView(board);
-        String str = st.nextToken();
-
-        boolean turn = true;
-
-        if (str.equals("시작")) {
-            gameView.print();
-            while(true){
-                st = new StringTokenizer(br.readLine());
-                String command = st.nextToken();
-                if (command.equals("move")) {
-                    String from = st.nextToken();
-                    String to = st.nextToken();
-                    if((turn && gameChess.findPiece(from).isWhite()) || (!turn && gameChess.findPiece(from).isBlack())){
-
-                    }else{
-                        throw new NotUserTurn("선택한 기물은 사용자의 기물이 아닙니다.");
-                    }
-                    gameChess.move(from, to);
-                    gameView.print();
-                } else if (command.equals("종료")) {
-                    break;
-                }
-            }
-        }
-
+        new ChessStarter().start();
 
     }
 }
---
+++ src/main/java/softeer2nd/chess/game/ChessStarter.java
@@ -0,0 +1,51 @@
+package softeer2nd.chess.game;
+
+import softeer2nd.chess.Board;
+import softeer2nd.chess.exception.NotUserTurn;
+import softeer2nd.chess.pieces.piecetype.Piece;
+import softeer2nd.chess.view.GameView;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static softeer2nd.chess.pieces.piecetype.Piece.*;
+import static softeer2nd.chess.pieces.piecetype.Piece.Color.*;
+
+public class ChessStarter {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = null;
+
+    Board board = null;
+    GameChess gameChess = null;
+    GameView gameView = null;
+
+    Color turn = WHITE;
+
+    public void start() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        board = new Board();
+        gameChess = new GameChess(board);
+        gameView = new GameView(board);
+        String str = st.nextToken();
+        
+        if (str.equals("시작")) {
+            gameView.print();
+            while(true){
+                st = new StringTokenizer(br.readLine());
+                String command = st.nextToken();
+                if (command.equals("move")) {
+                    String from = st.nextToken();
+                    String to = st.nextToken();
+                    if((turn != gameChess.findPiece(from).getColor())){
+                        throw new NotUserTurn("선택한 기물은 사용자의 기물이 아닙니다.");
+                    }
+                    gameChess.move(from, to);
+                    gameView.print();
+                } else if (command.equals("종료")) {
+                    break;
+                }
+            }
+        }
+
+    }
+}
